61401b8a8a854bf03ebb3e3a53701fe9414ba852
Nikolai Petukhov <petukhov.niko@gmail.com>
2023-02-07 14:56:53 -0300

    deleted opencv from dependencies

--- setup.py
+++ setup.py
@@ -15,7 +15,6 @@ INSTALL_REQUIRES = [
     "Pillow",
     "matplotlib",
     "scikit-image>=0.14.2",
-    "opencv-python-headless",
     "imageio<=2.6.1; python_version<'3.5'",
     "imageio; python_version>='3.5'",
     "Shapely"
